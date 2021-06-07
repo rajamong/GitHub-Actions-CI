db2d351df04feae786fdd4a5422ce47551287007
rajamong <63121185+rajamong@users.noreply.github.com>
2021-06-06 18:47:08 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import unittest
 # imports calculator class from calculator program
 from calculator import calculator
 
+
 class testCase(unittest.TestCase):
     def setUp(self):
         self.calculator = calculator()
@@ -10,11 +11,11 @@ class testCase(unittest.TestCase):
     # tests the addition functionality
     def test1(self):
         self.assertEqual(self.calculator.addition(10, 2), (12))
-    
+
     # tests the subtraction functionality
     def test2(self):
         self.assertEqual(self.calculator.subtraction(10, 5), (5))
-    
+
     # tests the multiplication functionality
     def test3(self):
         self.assertEqual(self.calculator.multiplication(10, 2), (20))
@@ -22,6 +23,7 @@ class testCase(unittest.TestCase):
     # tests the division functionality
     def test4(self):
         self.assertEqual(self.calculator.division(10, 5), (2))
+
 
 if __name__ == "__main__":
     unittest.main()
