e95931c6459ae8991fa2bade54f0d1e1a93c2838
rajamong <63121185+rajamong@users.noreply.github.com>
2021-06-06 18:42:57 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,16 +3,14 @@ class calculator:
 
     def __init__(self):
         pass
+
     
     def addition(self, num1, num2):
         return num1 + num2
 
     def subtraction(self, num1, num2):
         return num1 - num2
-
     def multiplication(self, num1, num2):
         return num1 * num2
-
     def division(self, num1, num2):
         return num1 / num2
-
